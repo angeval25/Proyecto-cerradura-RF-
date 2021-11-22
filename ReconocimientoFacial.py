a7e7bf2c3245c0c347c168db6752d97efe680cf9
angeval25 <90471252+angeval25@users.noreply.github.com>
2021-11-22 08:26:49 -0500

    Update ReconocimientoFacial.py

--- ReconocimientoFacial.py
+++ ReconocimientoFacial.py
@@ -1,17 +1,13 @@
 import cv2
 import os
 
-dataPath = 'C:/Users/angel/Pictures/Reconocimiento Facial/Data' #Cambia a la ruta donde hayas almacenado Data
+dataPath = 'C:/Users/angel/Pictures/Reconocimiento Facial/Data'
 imagePaths = os.listdir(dataPath)
 print('imagePaths=',imagePaths)
 
-#face_recognizer = cv2.face.EigenFaceRecognizer_create()
-#face_recognizer = cv2.face.FisherFaceRecognizer_create()
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 # Leyendo el modelo
-#face_recognizer.read('modeloEigenFace.xml')
-#face_recognizer.read('modeloFisherFace.xml')
 face_recognizer.read('modeloLBPHFace.xml')
 
 #cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
@@ -33,23 +29,7 @@ while True:
 		result = face_recognizer.predict(rostro)
 
 		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-		'''
-		# EigenFaces
-		if result[1] < 5700:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
 		
-		# FisherFace
-		if result[1] < 500:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		'''
 		# LBPHFace
 		if result[1] < 70:
 			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
